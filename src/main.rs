b6152e851a180f3cef7f01079e2414ea2e870d2b
Clemens Koza <clemens.koza@gmx.at>
2023-10-14 18:53:14 +0200

    add Embassy Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let _p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
 }
