f7bd7509f63c18e1fff9485d9e9b1107e88eff8f
Clemens Koza <clemens.koza@gmx.at>
2024-12-27 15:15:04 +0100

    a bit of cleanup of the SPI code

--- src/bin/spi.rs
+++ src/bin/spi.rs
@@ -6,48 +6,87 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::{
-    gpio,
     spi,
     time::Hertz,
-    Peripheral,
 };
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
-struct Adxl345<'d, T: spi::Instance, Rx: spi::RxDma<T>, Tx: spi::TxDma<T>, NCS: gpio::Pin> {
-    ncs: gpio::Output<'d, NCS>,
-    spi: spi::Spi<'d, T, Tx, Rx>,
-}
+pub mod adxl345 {
+    use embassy_stm32::{
+        gpio,
+        spi,
+        time::Hertz,
+        Peripheral,
+    };
 
-impl<'d, T: spi::Instance, Rx: spi::RxDma<T>, Tx: spi::TxDma<T>, NCS: gpio::Pin>
-Adxl345<'d, T, Rx, Tx, NCS>
-{
-    pub fn new(
-        spi: impl Peripheral<P = T> + 'd,
-        sck: impl Peripheral<P = impl spi::SckPin<T>> + 'd,
-        mosi: impl Peripheral<P = impl spi::MosiPin<T>> + 'd,
-        miso: impl Peripheral<P = impl spi::MisoPin<T>> + 'd,
-        rxdma: impl Peripheral<P = Rx> + 'd,
-        txdma: impl Peripheral<P = Tx> + 'd,
-        ncs: impl Peripheral<P = NCS> + 'd,
-    ) -> Self {
-        let ncs = gpio::Output::new(ncs, gpio::Level::High, gpio::Speed::Low);
-        let mut config = spi::Config::default();
-        config.mode = spi::MODE_3;
-        config.frequency = Hertz(1_000_000);
-        let spi = spi::Spi::new(spi, sck, mosi, miso, txdma, rxdma, config);
+    pub const READ: u8 = 1 << 7;
+    pub const MULTI: u8 = 1 << 6;
 
-        Self { ncs, spi }
+    pub struct Adxl345<'d, T: spi::Instance, Rx: spi::RxDma<T>, Tx: spi::TxDma<T>, NCS: gpio::Pin> {
+        ncs: gpio::Output<'d, NCS>,
+        spi: spi::Spi<'d, T, Tx, Rx>,
     }
 
-    pub async fn transfer(&mut self, data: &mut [u8]) -> Result<(), spi::Error> {
-        self.ncs.set_low();
-        self.spi.transfer_in_place(data).await?;
-        self.ncs.set_high();
+    impl<'d, T: spi::Instance, Rx: spi::RxDma<T>, Tx: spi::TxDma<T>, NCS: gpio::Pin>
+    Adxl345<'d, T, Rx, Tx, NCS>
+    {
+        pub fn new(
+            spi: impl Peripheral<P = T> + 'd,
+            sck: impl Peripheral<P = impl spi::SckPin<T>> + 'd,
+            mosi: impl Peripheral<P = impl spi::MosiPin<T>> + 'd,
+            miso: impl Peripheral<P = impl spi::MisoPin<T>> + 'd,
+            rxdma: impl Peripheral<P = Rx> + 'd,
+            txdma: impl Peripheral<P = Tx> + 'd,
+            ncs: impl Peripheral<P = NCS> + 'd,
+        ) -> Self {
+            let ncs = gpio::Output::new(ncs, gpio::Level::High, gpio::Speed::Low);
+            let mut config = spi::Config::default();
+            config.mode = spi::MODE_3;
+            config.frequency = Hertz(1_000_000);
+            let spi = spi::Spi::new(spi, sck, mosi, miso, txdma, rxdma, config);
 
-        Ok(())
+            Self { ncs, spi }
+        }
+
+        pub async fn transfer(&mut self, data: &mut [u8]) -> Result<(), spi::Error> {
+            self.ncs.set_low();
+            self.spi.transfer_in_place(data).await?;
+            self.ncs.set_high();
+
+            Ok(())
+        }
+
+        pub async fn read<const N: usize>(&mut self, address: u8) -> Result<[u8; N], spi::Error> {
+            let mut address = address | READ;
+            if N != 1 {
+                address |= MULTI;
+            }
+
+            self.ncs.set_low();
+            self.spi.write(&[address]).await?;
+            let mut buf = [0u8; N];
+            self.spi.read(&mut buf).await?;
+            self.ncs.set_high();
+
+            Ok(buf)
+        }
+
+        pub async fn write(&mut self, address_data: &mut [u8]) -> Result<(), spi::Error> {
+            if address_data.len() != 2 {
+                address_data[0] |= MULTI;
+            }
+
+            self.ncs.set_low();
+            self.spi.write(address_data).await?;
+            self.ncs.set_high();
+
+            Ok(())
+        }
     }
 }
+
+use adxl345::Adxl345;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -62,29 +101,20 @@ async fn main(_spawner: Spawner) {
     // let spi = Spi::new(p.SPI1, p.PB3, p.PB5, p.PB4, p.DMA1_CH3, p.DMA1_CH2, config);
     let mut accel = Adxl345::new(p.SPI1, p.PB3, p.PB5, p.PB4, p.DMA1_CH2, p.DMA1_CH3, p.PD6);
 
-    const READ: u8 = 1 << 7;
-    const MULTI: u8 = 1 << 6;
-
-    let mut buf = [0u8; 7];
-
-    buf[0] = 0 | READ;
-    unwrap!(accel.transfer(&mut buf[..2]).await);
-    info!("Device ID: {:X}", buf[1]);
+    let [id] = unwrap!(accel.read(0x00).await);
+    info!("Device ID: {:X}", id);
 
     Timer::after(Duration::from_micros(1)).await;
 
-    buf[0] = 0x2D;
-    buf[1] = 0x08;
-    unwrap!(accel.transfer(&mut buf[..2]).await);
+    unwrap!(accel.write(&mut [0x2D, 0x08]).await);
 
     Timer::after(Duration::from_micros(1)).await;
 
     for _ in 0u32.. {
-        buf[0] = 0x32 | READ | MULTI;
-        unwrap!(accel.transfer(&mut buf[..7]).await);
-        let x = i16::from_le_bytes(buf[1..3].try_into().unwrap());
-        let y = i16::from_le_bytes(buf[3..5].try_into().unwrap());
-        let z = i16::from_le_bytes(buf[5..7].try_into().unwrap());
+        let data = unwrap!(accel.read::<6>(0x32).await);
+        let x = i16::from_le_bytes(data[0..2].try_into().unwrap());
+        let y = i16::from_le_bytes(data[2..4].try_into().unwrap());
+        let z = i16::from_le_bytes(data[4..6].try_into().unwrap());
         info!("Acceleration: ({}, {}, {})", x, y, z);
 
         Timer::after(Duration::from_millis(200)).await;
